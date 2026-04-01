6d5a9625080f1e4f1e6638d158ce26a6b871d177
np03cs4a240382-create <np03cs4a240382@heraldcollege.edu.np>
2026-04-01 09:56:22 +0545

    Create create_users_table.sql

---
+++ New collab and developmen/create_users_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_users_email ON users(email);
